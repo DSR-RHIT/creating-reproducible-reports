959899cdcbd1249477f57147a4abfe6c677dfe4e
graphdr <graphdoctor@gmail.com>
2016-06-16 14:44:25 -0400

    updated license

--- scripts/cleanup-files.R
+++ scripts/cleanup-files.R
@@ -1,54 +1,21 @@
-#-----------------------------------------------------------
-#
-#   define-latex-colors.R
-#
-#   2013-08-18
-#   RAL
-#
-#-----------------------------------------------------------
+# removing extraneous tex files from folder
 
-library(RColorBrewer)
-library(stringr)
+unlink("print-pages/*.log")
+unlink("print-pages/*.gz")
+unlink("print-pages/*.tex")
+unlink("print-pages/.Rhistory")
 
-# brown/blue-green (div)
-BrBG <- brewer.pal(8,"BrBG")
-# remove hashtag and quotes to use in LaTeX xcolor package
-BrBG <- noquote(str_replace(BrBG, '#', ''))
-darkBr  = BrBG[1]
-medBr   = BrBG[2]
-lightBr = BrBG[3]
-paleBr  = BrBG[4]
-paleBG  = BrBG[5]
-lightBG = BrBG[6]
-medBG   = BrBG[7] 
-darkBG  = BrBG[8]
+unlink("slides/*.log")
+unlink("slides/*.gz")
+unlink("slides/*.tex")
+unlink("slides/*.nav")
+unlink("slides/*.vrb")
+unlink("slides/*.toc")
+unlink("slides/*.snm")
+unlink("slides/.Rhistory")
 
-# purple-green (div)
-PRGn <- brewer.pal(8,"PRGn")
-PRGn <- noquote(str_replace(PRGn, '#', ''))
-darkPR  = PRGn[1]
-medPR   = PRGn[2]
-lightPR = PRGn[3]
-palePR  = PRGn[4]
-paleGn  = PRGn[5]
-lightGn = PRGn[6]
-medGn   = PRGn[7] 
-darkGn  = PRGn[8]
 
-# grays
-Grays <- brewer.pal(8,"Greys")
-Grays <- noquote(str_replace(Grays, '#', ''))
-gray1 = Grays[1]
-gray2 = Grays[2]
-gray3 = Grays[3]
-gray4 = Grays[4]
-gray5 = Grays[5] 
-gray6 = Grays[6]
-gray7 = Grays[7] 
-gray8 = Grays[8]
-
-# last line   
-
+unlink(".Rhistory") # in the main directory
 
 # License BSD-3-clause
 #
@@ -64,5 +31,4 @@ gray8 = Grays[8]
 # 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 # 
 # This software is provided by the copyright holders and contributors "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. In no event shall the copyright holder or contributors be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
-
 
