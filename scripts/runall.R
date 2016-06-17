aa42605e96fd036817dcc42cdd38653a0b45dd1f
graphdr <graphdoctor@gmail.com>
2016-06-17 16:37:10 -0400

    runall

--- scripts/runall.R
+++ scripts/runall.R
@@ -4,7 +4,13 @@
 # used by all .md files in the pages directroy
 library(knitr)
 opts_chunk$set(include=TRUE, echo=FALSE, message=FALSE)
-
 knit('pages/agenda.Rmd', output = 'pages/agenda.md')
 
+# cleanup, main directory
+unlink(".Rhistory")
 
+# cleanup, pages directory
+unlink("pages/*.html")
+
+# cleanup, reports directory
+unlink("reports/*.html")
