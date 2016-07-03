6542c2c5c4b0bf31bb8bebd6ad8a7c5e21741177
graphdr <graphdoctor@gmail.com>
2016-07-03 14:28:51 -0400

    fixing pages.md

--- runall.R
+++ runall.R
@@ -31,6 +31,9 @@ sapply(Rmd_scripts, function(x) render(x))
 # Rmds -> mds with edited YAML header for gh-pages
 source('scripts/helper-01_create-gh-pages.R')
 
+# clear old md files from pages dir
+unlink("pages/*.md")
+
 # plyr::failwith() allows completion even if an error occurs
 sapply(Rmd_scripts, failwith(NULL, Rmd_to_gh_pages))
 
